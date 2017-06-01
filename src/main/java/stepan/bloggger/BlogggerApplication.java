2834d045733e351a4cece3b883259856f6533e90
Stepan Razdobarov <northernpeople@gmail.com>
2017-05-31 22:09:48 -0700

    added view security

--- src/main/java/stepan/bloggger/BlogggerApplication.java
+++ src/main/java/stepan/bloggger/BlogggerApplication.java
@@ -2,7 +2,9 @@ package stepan.bloggger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,5 +12,10 @@ public class BlogggerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogggerApplication.class, args);
+	}
+	
+	@Bean
+	public SpringSecurityDialect securityDialect() {
+	  return new SpringSecurityDialect();
 	}
 }
